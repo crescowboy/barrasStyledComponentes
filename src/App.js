0c26b8c44693add86df384fb1ab0949842798dc9
wcrespo <wellingtoncrespo2001@gmail.com>
2023-04-26 12:06:32 -0500

    cambios 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Titulo,Moverse,Sombra,Contenedor } from "./estilos";
 function App() {
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Contenedor>
+      <Titulo elColor="red" redondo>1</Titulo>
+      <Titulo elColor="yellow">2</Titulo>
+      <Titulo>3</Titulo>
+      </Contenedor>
+      <Moverse distancia={333} segundos={12}></Moverse>
+      <Moverse distancia={111} segundos={6}></Moverse>
+      <Moverse distancia={222} segundos={10}></Moverse>
+      <Moverse distancia={33} segundos={8}></Moverse>
+      <Sombra></Sombra>
+    </>
   );
 }
 
---
+++ src/estilos.js
@@ -0,0 +1,46 @@
+import styled, {css, keyframes} from "styled-components";
+
+
+const  miColor="black";
+
+  const animacion = (valor)=>keyframes`
+  0%{
+    transform:rotate(0deg)
+  }
+  100%{
+    transform:rotate(${valor}deg)
+  }
+  `
+
+  export const Titulo=styled.div`
+    width:100px;
+    height:100px;
+    background-color:${({elColor})=>elColor ? elColor : "silver"};
+    margin:2px;
+    
+    ${props => props.redondo &&
+    css`
+      border-radius:50%;
+      border:2px solid black;
+    `}
+    &:hover{
+      border: 1px solid ${miColor};
+      animation:${animacion} 3s ease-in-out;
+    animation-fill-mode:forwards;
+    }
+  `
+
+  export const Sombra = styled(Titulo)`
+    box-shadow:0 0 20px red;
+  `
+  export const Contenedor=styled.div`
+    display:flex;
+  `
+  export const Moverse=styled.div`
+    width: 200px;
+    height:100px;
+    background-color:blue;
+    animation: ${(props) => animacion(props.distancia)} 
+                ${(props) => props.segundos}s ease-out;
+    animation-fill-mode:forwards;
+  `
