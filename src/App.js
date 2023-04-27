3d84e69920d93260056a0544791924f1f28b36e2
wcrespo <wellingtoncrespo2001@gmail.com>
2023-04-26 22:56:55 -0500

    logica final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import React from "react";
-import { Titulo, Moverse, Sombra, Contenedor, Caja } from "./estilos";
+import React, { useState } from "react";
+import { Titulo, Moverse, Sombra, Contenedor, Caja, Barra, Linea } from "./estilos";
 import { valores } from "./data/datos";
 
 function App() {
+
+  const [datos,setDatos] = useState(valores);
+
+  
+
+  const click=(altura,indice)=>{
+    setDatos(
+      datos.map((item,index)=>{
+        if(index===indice){
+          return({...item,altura:
+            (item.altura<175)
+             ? item.altura+40
+              :item.altura,
+              desde:item.altura
+            })
+        }else{
+          return ({...item,desde:item.altura});
+        }
+      }
+    ))
+  }
+
   return (
     <>
-    <div>
+    
      <Titulo>Tus ganacias: </Titulo>
-     <Contenedor>
-      {valores.map((color,index)=>(
-        <div key={index}>
+     <Caja>
+      {datos.map((color,index)=>(
+        
          
-         
-            <Caja elColor={color.color}></Caja>
+            <Barra key={index} onClick={()=>click(color.altura,index)} color={color.color} desde={color.desde} altura={color.altura}></Barra>
             
-         
-        </div>
+      
 
       ))}
-       </Contenedor>
-       </div>
+      <Linea></Linea>
+      </Caja>
     </>
   );
 }
--- src/estilos.js
+++ src/estilos.js
@@ -1,49 +1,72 @@
 import styled, {css, keyframes} from "styled-components";
+import { valores } from "./data/datos";
 
 
 const  miColor="black";
 
-  const animacion = (valor)=>keyframes`
-  0%{
-    transform:rotate(0deg)
-  }
-  100%{
-    transform:rotate(${valor}deg)
-  }
-  `
+const unColor=()=>{
+  const colores=["gold","violet","orangered","navy","red"]
+  return colores[Math.floor(Math.random()*colores.length)]
+}
+
+  
 
   export const Titulo=styled.div`
-    font-size: 60px;
-    text-align: center;
+    font-size: 3em;
+    margin-bottom:30px;
+    color:${unColor};
   `
 
-  export const Caja=styled.div`
-    width:100px;
-    height:100px;
-    background-color:${({elColor})=>elColor ? elColor : "silver"};
-    margin:8px;
+  
+  
+
+  const animacion = (desde,valor)=>keyframes`
+  from {
+    height: ${desde}px;
     
-    ${props => props.redondo &&
-    css`
-      border-radius:50%;
-      border:2px solid black;
-    `}
-    &:hover{
-      border: 1px solid ${miColor};
-      animation:${animacion} 3s ease-in-out;
-    animation-fill-mode:forwards;
-    }
+  }
+  to {
+    height: ${valor}px;
+  }
+  
+  `
+  export const Linea=styled.div`
+    position:absolute;
+    border-top: 1px dashed black;
+    width:300px;
+    height:1px;
+    left:15px;
+    bottom:186px;
   `
 
-  export const Sombra = styled(Titulo)`
-    box-shadow:0 0 20px red;
+  export const Barra=styled.div`
+      width:50px;
+      background-color:${(props)=>props.color ? props.color : "silver"};
+      margin-right:10px;
+      animation:${(valor)=>animacion(valor.desde,valor.altura)} 1s ease-in-out;
+      animation-fill-mode:forwards;
+  `
+  
+  export const Caja=styled.div`
+    display:flex;
+    width:300px;
+    height:200px;
+    position:relative;
+    align-items:flex-end;
+    border-bottom: 3px solid black;
+    border-left:4px solid gray;
+    padding:10px;
+    // animation:${animacion} 2s ease-in-out;
+    
+    
+    
   `
   export const Contenedor=styled.div`
     display:flex;
     justify-content: center;
     align-items: center;
     height: 100vh;
-    background-color:red;
+    
   `
   export const Moverse=styled.div`
     width: 200px;
